ce0d24e7f741d294fd4b407f3a7dc5fe051fb22a
nirvana <nirvana@DESKTOP-PM8E54L>
2017-09-29 17:44:01 +0800

    服务

--- src/main/java/com/huawei/cnpc/controller/SampleController.java
+++ src/main/java/com/huawei/cnpc/controller/SampleController.java
@@ -1,17 +1,28 @@
 package com.huawei.cnpc.controller;
 
+import java.util.List;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.*;
-import org.springframework.boot.autoconfigure.*;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.stereotype.*;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
-@EnableAutoConfiguration
+@EnableDiscoveryClient
 public class SampleController {
+	private final Logger logger = Logger.getLogger(getClass());
+	@Autowired
+	private DiscoveryClient client;
 
-	@RequestMapping("/")
-	@ResponseBody
-	String home() {
+	@RequestMapping(value = "/hello", method = RequestMethod.GET)
+	public String home() {
+		List<String> list = client.getServices();
+		for (String v : list) {
+			logger.info("service:" + v);
+		}
 		return "Hello World!";
 	}
 
