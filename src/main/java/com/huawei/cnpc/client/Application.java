347f40a7126d3a1d2963fcae6a64b017b2816525
nirvana <nirvana@DESKTOP-PM8E54L>
2017-09-29 14:34:56 +0800

    spring cloud 注册中心

--- src/main/java/com/huawei/cnpc/client/Application.java
+++ src/main/java/com/huawei/cnpc/client/Application.java
@@ -1,4 +1,4 @@
-package com.huawei.cnpc;
+package com.huawei.cnpc.client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ src/main/java/com/huawei/cnpc/server/EurekaServer.java
@@ -0,0 +1,14 @@
+package com.huawei.cnpc.server;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServer {
+
+	public static void main(String[] args) {
+		new SpringApplicationBuilder(EurekaServer.class).run(args);
+	}
+}
