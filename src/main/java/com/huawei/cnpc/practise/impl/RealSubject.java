8828cf88a7cafb7cb8798c8804f78724acd82240
hadoop <hadoop@hadoop-ThinkPad-E450>
2017-10-02 11:48:13 +0800

    动态代理

---
+++ src/main/java/com/huawei/cnpc/practise/impl/RealSubject.java
@@ -0,0 +1,19 @@
+package com.huawei.cnpc.practise.impl;
+
+import com.huawei.cnpc.practise.interf.Subject;
+
+public class RealSubject implements Subject {
+
+	@Override
+	public void rent() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void hello(String str) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/main/java/com/huawei/cnpc/practise/interf/Subject.java
@@ -0,0 +1,6 @@
+package com.huawei.cnpc.practise.interf;
+
+public interface Subject {
+	public void rent();
+	public void hello(String str);
+}
