4dc092fc142f8ee4f0fc94859cff2ec41547d5bf
nirvana <nirvana@DESKTOP-PM8E54L>
2017-10-20 10:19:03 +0800

    注释

--- cnpc-client/src/main/java/org/cnpc/Application.java
+++ cnpc-client/src/main/java/org/cnpc/Application.java
@@ -5,7 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
- * 服务发现中心 注意application路径问题
+ * 服务发现中心 注意application路径问题《controller需要和application在同一目录下,如果不在同一目录下
+ * 对应的controller就无法被自动发现》
  * 
  * @author nirvana
  *
